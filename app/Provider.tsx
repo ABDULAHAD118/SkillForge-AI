03cbe913440d73f4c0e6397be5c2f86d53417bd9
ABDULAHAD118 <129290085+ABDULAHAD118@users.noreply.github.com>
2025-06-09 19:33:07 +0500

    Complete setup of database

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,22 +1,30 @@
 "use client";
-import React, { ReactNode, useEffect } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import axios from "axios";
 import { useUser } from "@clerk/nextjs";
+import { UserDetailContext } from "@/context/UserDetailContext";
 
 const Provider = ({ children }: { children: ReactNode }) => {
   const { user } = useUser();
+  const [userDetail, setUserDetail] = useState<any>(null);
+
   const CreateNewUSer = async () => {
     const result = await axios.post("/api/user", {
       name: user?.fullName,
       email: user?.primaryEmailAddress?.emailAddress,
     });
     console.log(result.data);
+    setUserDetail(result.data);
   };
 
   useEffect(() => {
     user && CreateNewUSer();
   }, [user]);
-  return <div>{children}</div>;
+  return (
+    <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+      <div>{children}</div>;
+    </UserDetailContext.Provider>
+  );
 };
 
 export default Provider;
--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -10,13 +10,11 @@ export async function POST(req: NextRequest) {
     .select()
     .from(usersTable)
     .where(eq(usersTable.email, email));
-
   if (users?.length == 0) {
     const result = await db
       .insert(usersTable)
       .values({ name: name, email: email })
       .returning(usersTable);
-    console.log(result);
     return NextResponse.json(result);
   }
   return NextResponse.json(users[0]);
